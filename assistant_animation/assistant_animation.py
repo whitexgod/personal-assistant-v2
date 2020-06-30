7462f81e816cc96702199270568d860488db8dd3
Tuhin Mukherjee <59216687+whitexgod@users.noreply.github.com>
2020-06-30 19:36:55 +0530

    commit1

---
+++ assistant_animation/assistant_animation.py
@@ -0,0 +1,26 @@
+import pyglet
+
+def animation():
+
+    animation = pyglet.image.load_animation("assistant_image.gif")
+    animSprite = pyglet.sprite.Sprite(animation)
+
+    w = animSprite.width
+    h = animSprite.height
+
+    window = pyglet.window.Window(width=w, height=h)
+
+    r,g,b,alpha = 0.5,0.5,0.8,0.5
+
+    pyglet.gl.glClearColor(r,g,b,alpha)
+
+    @window.event
+    def on_draw():
+        window.clear()
+        animSprite.draw()
+
+
+    pyglet.app.run()
+
+
+animation()
